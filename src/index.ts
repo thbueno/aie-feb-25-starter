3f78ac120996c1eff9e59911e5da987212aa9a9c
thbueno <thiagobueno.s@gmail.com>
2025-05-25 12:00:00 +0700

    commit catch up

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-import { openai } from '@ai-sdk/openai'
-import { generateText } from 'ai'
-import 'dotenv/config'
+import { openai } from "@ai-sdk/openai";
+import { generateText, tool } from "ai";
+import "dotenv/config";
+import { z } from "zod";
  
 const main = async () => {
-  const result = await generateText({
-    model: openai('gpt-4o-mini'),
-    messages: [
-      {
-        role: 'user',
-        content: 'You are a helpful assistant that can answer questions and help with tasks.',
+    const result = await generateText({
+      model: openai("gpt-4o-mini"),
+      prompt: "What's 10 + 5?",
+      tools: {
+        addNumbers: tool({
+          description: "Add two numbers together",
+          parameters: z.object({
+            num1: z.number(),
+            num2: z.number(),
+          }),
+          execute: async ({ num1, num2 }) => {
+            return num1 + num2;
+          },
+        }),
       },
-    ],
-  })
-  console.log(result.text)
-}
- 
-main()
+    });
+    console.log(result.toolResults);
+  };
+   
+  main();

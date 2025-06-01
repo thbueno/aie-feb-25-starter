b7fbacce580fabdcc5e7a378f71454e9eff890c0
thbueno <thiagobueno.s@gmail.com>
2025-06-01 18:59:33 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import "dotenv/config";
-
-console.log("Hello, world!")
+import { openai } from '@ai-sdk/openai'
+import { generateText } from 'ai'
+import 'dotenv/config'
+ 
+const main = async () => {
+  const result = await generateText({
+    model: openai('gpt-4o-mini'),
+    prompt: 'Hello, world!',
+  })
+  console.log(result.text)
+}
+ 
+main()

b9fbd452de13395d601646d84f408b070083a887
thbueno <thiagobueno.s@gmail.com>
2025-06-02 09:18:41 +0700

    Wether agent struture

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,52 @@
-import { openai } from "@ai-sdk/openai";
-import { generateText, tool } from "ai";
-import "dotenv/config";
-import { z } from "zod";
+import { openai } from '@ai-sdk/openai'
+import { generateText, Output, tool } from 'ai'
+import 'dotenv/config'
+import { z } from 'zod'
  
 const main = async () => {
-    const result = await generateText({
-      model: openai("gpt-4o-mini"),
-      prompt: "What's 10 + 5?",
-      maxSteps: 2,
-      tools: {
-        addNumbers: tool({
-          description: "Add two numbers together",
-          parameters: z.object({
-            num1: z.number(),
-            num2: z.number(),
-          }),
-          execute: async ({ num1, num2 }) => {
-            return num1 + num2;
-          },
+  const result = await generateText({
+    model: openai('gpt-4o-mini'),
+    prompt: 'Get the weather in SF and NY, then add them together.',
+    maxSteps: 2,
+    tools: {
+      addNumbers: tool({
+        description: 'Add two numbers together',
+        parameters: z.object({
+          num1: z.number(),
+          num2: z.number(),
         }),
-      },
-    });
-    console.log(result.text);
-    console.log(JSON.stringify(result.steps, null, 2));
-  };
-   
-  main();
+        execute: async ({ num1, num2 }) => {
+          return num1 + num2
+        },
+      }),
+      getWeather: tool({
+        description: 'Get the current weather at a location',
+        parameters: z.object({
+          latitude: z.number(),
+          longitude: z.number(),
+          city: z.string(),
+        }),
+        execute: async ({ latitude, longitude, city }) => {
+          const response = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode,relativehumidity_2m&timezone=auto`
+          )
+ 
+          const weatherData = await response.json()
+          return {
+            temperature: weatherData.current.temperature_2m,
+            weatherCode: weatherData.current.weathercode,
+            humidity: weatherData.current.relativehumidity_2m,
+            city,
+          }
+        },
+      }),
+    },
+    experimental_output: Output.object({
+      schema: z.object({ sum: z.string() }),
+    }),
+  })
+  console.log(result.steps.length)
+  console.log(result.experimental_output)
+}
+ 
+main()
